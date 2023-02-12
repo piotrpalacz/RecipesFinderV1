e468359659068469a406b3ca7985fbe2bf3821d8
Piotr Palacz <peterpalacz12@gmail.com>
2023-02-12 23:44:40 +0100

    Index page as login page

--- ExternalAPFI/Controllers/HomeController.cs
+++ ExternalAPFI/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    public IActionResult LoginStart()
+    {
+        return View();
+    }
+
     public IActionResult Index()
     {
         return View();
