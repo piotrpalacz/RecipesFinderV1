b2581b5208f4b8c2540e1d7977f07d2d689ff95b
Piotr Palacz <peterpalacz12@gmail.com>
2023-02-12 19:47:58 +0100

    Let's add some files

---
+++ ExternalAPFI.Data/Data/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExternalAPFI.Data.Data
+{
+    public class AppUser : IdentityUser
+    {
+        [PersonalData]
+        public string? FullName { get; set; }
+
+        [PersonalData]
+        public DateTime DOB { get; set; }
+
+        //[PersonalData]
+        //public Uri GithubLink { get; set; }
+
+        //[PersonalData]
+        //public Uri LinkedInLink { get; set; }
+
+    }
+}
+
---
+++ ExternalAPFI.Data/Data/AppUserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExternalAPFI.Data.Data
+{
+    public class AppUserDbContext : IdentityDbContext<AppUser>
+    {
+        public AppUserDbContext(DbContextOptions<AppUserDbContext> options) : base(options)
+        {
+        }
+    }
+}
+
